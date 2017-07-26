5e2662d35772c75eb4a7efbf97cbbd52e64f0980
Yuukaze <jeremiahbacoy1292@gmail.com>
2017-07-26 14:08:31 +0800

    added comments

--- weebsave.py
+++ weebsave.py
@@ -10,6 +10,11 @@ consumer_secret = 'sAuWJUuFOJeORK0JrgknP4FKqml9dMOjA4TVjiJ3C9qFIZcB6t'
 access_token = '1374751998-Olqap3rLJEMb7a8v9KUmNNVtqTeYrc4K1c00dSt'
 access_secret = 'jDXMAUlr2yhkP73ZesZzcupQMOJWStciZG6zNcoZwPYJ6'
 
+#This snippet of code sends the gathered data to the client socket. 
+#The data is sent in Json format and encoded in UTF - 8. 
+#It also states the status of the data transfer, 
+#whether it was successful or not.
+
 class TweetsListener(StreamListener):
 
   def __init__(self, csocket):
@@ -29,15 +34,32 @@ class TweetsListener(StreamListener):
       print(status)
       return True
 
+
+#This block of code is the one responsible for the connection to twitter 
+#API. It requires you to give authentication, this serves the purpose 
+#of connecting the module to the twitter apps and, in turn allow 
+#streaming.
+
 def sendData(c_socket):
   auth = OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_secret)
   twitter_stream = Stream(auth, TweetsListener(c_socket))
   twitter_stream.filter(track=["bnha","boku no hero academia","my hero academia","bokunoheroacademia","myheroacademia","boku-no-hero-academia","my-hero-academia"])
 
+
+
+#This block of code is responsible for setting a socket in the 
+#given IP address to a be passive socket that would only listen to 
+#request. This is done by binding the host IP address and the specified 
+#socket to the variable s. This means when the program is ran it won’t 
+#automatically stream but rather would wait for a request using the 
+#function start(). This block of code also would not work  if the program 
+#where this is located was not the entry point of this activation. 
+#Listen also has a queue of 5. 
+
 if __name__ == "__main__":
   s = socket.socket()         # Create a socket object
-  host = "10.115.20.58"      # Get local machine name
+  host = "10.115.10.72"      # Get local machine name
   port = 5555                 # Reserve a port for your service.
   s.bind((host, port))        # Bind to the port
 
